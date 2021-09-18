cc4346e00e0da257afe6cd903a30ecf74fe16eee
Curtis Dean <cdean.atx@gmail.com>
2021-09-18 16:00:08 -0500

    Added Deliverable 2 files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -25,3 +25,18 @@ SELECT count(emp_no) AS ct,
 FROM unique_titles
 GROUP BY title
 ORDER BY ct DESC;
+
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_employee AS de ON e.emp_no = de.emp_no
+INNER JOIN titles AS ti ON e.emp_no = ti.emp_no
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
